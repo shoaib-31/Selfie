6b4c57823714e47c8e95f95a3c42c25036252094
Shoaib Akhtar <shoaibakmasood@gmail.com>
2024-07-22 18:57:42 +0530

    model loading

--- app/(main)/index.tsx
+++ app/(main)/index.tsx
@@ -5,12 +5,25 @@ import {
   Camera,
   useCameraDevice,
   useCameraPermission,
+  useFrameProcessor,
   useMicrophonePermission,
 } from "react-native-vision-camera";
+import { runOnJS } from "react-native-reanimated";
 import LinkButton from "@/components/LinkButton";
 import { CameraSide } from "@/constants/Enums";
-
+import { loadTensorflowModel } from "react-native-fast-tflite";
+import { useResizePlugin } from "vision-camera-resize-plugin";
 export default function Index() {
+  //States
+
+  const [model, setModel] = React.useState<any>(null);
+  const [isModelReady, setIsModelReady] = React.useState(false);
+
+  // Fucntions
+  const { resize } = useResizePlugin();
+
+  // Permissions
+
   const {
     hasPermission: cameraPermission,
     requestPermission: requestCameraPermission,
@@ -20,13 +33,23 @@ export default function Index() {
     requestPermission: requestMicrophonePermission,
   } = useMicrophonePermission();
   const device = useCameraDevice(CameraSide.Front);
-
   React.useEffect(() => {
     if (!cameraPermission) {
       requestCameraPermission();
     }
     if (!microphonePermission) {
       requestMicrophonePermission();
+    }
+    console.log("Permissions granted");
+    if (!isModelReady) {
+      (async () => {
+        const loadedModel = await loadTensorflowModel(
+          require("../../assets/face_detection_front.tflite")
+        );
+        console.log("Model Loaded");
+        setModel(loadedModel);
+        setIsModelReady(true);
+      })();
     }
   }, [cameraPermission, microphonePermission]);
 
@@ -37,6 +60,50 @@ export default function Index() {
       Linking.openSettings();
     }
   };
+
+  // Frame Processor
+
+  const frameProcessor = useFrameProcessor(
+    (frame: any) => {
+      "worklet";
+
+      if (!isModelReady || !model) return;
+
+      const resized = resize(frame, {
+        scale: {
+          width: 192,
+          height: 192,
+        },
+        pixelFormat: "rgb",
+        dataType: "uint8",
+      });
+
+      try {
+        const outputs = model.runSync([resized]);
+        const detection_boxes = outputs[0];
+        const detection_classes = outputs[1];
+        const detection_scores = outputs[2];
+        const num_detections = outputs[3];
+        // console.log(outputs);
+
+        console.log(`Detected ${num_detections[0]} objects!`);
+
+        for (let i = 0; i < detection_boxes.length; i += 4) {
+          const confidence = detection_scores[i / 4];
+          if (confidence > 0.7) {
+            const left = detection_boxes[i];
+            const top = detection_boxes[i + 1];
+            const right = detection_boxes[i + 2];
+            const bottom = detection_boxes[i + 3];
+            console.log(
+              `Object detected at ${left}, ${top}, ${right}, ${bottom}`
+            );
+          }
+        }
+      } catch (error: any) {}
+    },
+    [model]
+  );
 
   if (cameraPermission === null || microphonePermission === null) {
     return <Text>Checking permissions...</Text>;
@@ -57,11 +124,18 @@ export default function Index() {
 
   if (device == null) return <Text>No Camera Device Found</Text>;
 
+  // Camera
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.view}>
         <Text style={styles.title}>Camera</Text>
-        <Camera style={styles.camera} device={device} isActive={true} />
+        <Camera
+          style={styles.camera}
+          frameProcessor={frameProcessor}
+          device={device}
+          isActive={true}
+        />
         <LinkButton route="feed" title="Feed" />
       </View>
     </SafeAreaView>
